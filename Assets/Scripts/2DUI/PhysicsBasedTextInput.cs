b03c7dbc30293dafdb4201080eeb370e8667f856
BATMAN\lewis <lewis.wrigley@gmail.com>
2023-10-23 21:00:20 +0100

    rework physics based input field

--- Assets/Scripts/2DUI/PhysicsBasedTextInput.cs
+++ Assets/Scripts/2DUI/PhysicsBasedTextInput.cs
@@ -8,78 +8,41 @@ using System.Collections.Generic;
 namespace UIDemo {
 
   public class PhysicsBasedTextInput : PhysicsBasedUIElement {
+    [SerializeField] Transform _textTransform;
     [SerializeField] TMP_Text _textElement;
+    [SerializeField] float _rotationStrength = 5f;
+    [SerializeField] float _scaleStrength = 0.03f;
 
     private string _currentText;
-    private List<AnimatableCharacter> _charAnims;
-    private TMP_TextInfo _textInfo;
-    private TMP_MeshInfo[] _cachedMeshInfo;
-    private bool _hasTextChanged;
 
     void Start() {
       _currentText = "";
-      _charAnims = new List<AnimatableCharacter>();
       TMPro_EventManager.TEXT_CHANGED_EVENT.Add(OnTextChanged);
-
-      _textElement.ForceMeshUpdate();
-      _textInfo = _textElement.textInfo;
-      _cachedMeshInfo = _textInfo.CopyMeshInfoVertexData();
     }
 
     void OnDestroy() {
       TMPro_EventManager.TEXT_CHANGED_EVENT.Remove(OnTextChanged);
     }
 
-    void Update() {
-      if (_hasTextChanged) {
-        _cachedMeshInfo = _textInfo.CopyMeshInfoVertexData();
-        for (int i = 0; i < _textInfo.characterCount; i++) {
-          var charInfo = _textInfo.characterInfo[i];
-          int materialIndex = charInfo.materialReferenceIndex;
-          int vertexIndex = charInfo.vertexIndex;
-          Debug.Log($"i: {i}");
-          Debug.Log($"vertexIndex: {vertexIndex}");
-
-          var charAnim = new AnimatableCharacter(vertexIndex);
-          _charAnims.Add(charAnim);
-
-          // Animate($"character_{i}_scale",
-          //   1,
-          //   charAnim.Get,
-          //   charAnim.Set
-          // );
-        }
-        _currentText = _textElement.text;
-        _hasTextChanged = false;
-      }
-    }
-
     private void OnTextChanged(Object element) {
       if (element == _textElement && _textElement.text != _currentText) {
-        _hasTextChanged = true;
+        _currentText = _textElement.text;
+        Nudge("scale",
+          _scaleStrength,
+          () => { return transform.localScale.x; },
+          (value) => { transform.localScale = new Vector3(value, value, value); }
+        );
+        Nudge("text_scale",
+          -_scaleStrength * 2,
+          () => { return _textTransform.localScale.x; },
+          (value) => { _textTransform.localScale = new Vector3(value, value, value); }
+        );
+        Nudge("rotation",
+          Random.value * _rotationStrength * 2 - _rotationStrength,
+          () => { return Mathf.Asin(transform.up.x) * Mathf.Rad2Deg; },
+          (value) => { transform.rotation = Quaternion.Euler(0, 0, -value); }
+        );
       }
-    }
-
-    private class AnimatableCharacter {
-      public int firstVertexIndex { get; private set; }
-      public int currentScale { get; set; }
-
-      public AnimatableCharacter(int firstVertexIndex) {
-        firstVertexIndex = firstVertexIndex;
-        currentScale = 0;
-      }
-
-      // public float Get() {
-      //   return _currentScale;
-      // }
-
-      // public void Set(float value) {
-      //   _currentScale = value;
-      //   for (int i = 0; i < _originalVertices.Length; i++) {
-      //     var distFromCentre = _originalVertices[i] - _centre;
-      //     _meshVerticesRef[_firstVertexIndex + i] = _centre + distFromCentre * _currentScale;
-      //   }
-      // }
     }
   }
 }
